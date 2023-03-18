07824b88dcca201489b4b5625505411adf4ea328
kshipraDewat <kshipradewat66@gmail.com>
2023-03-18 09:43:04 +0530

    changed the author section which was a select an option field and made it an input field!!

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 const Create = () => {
     const [title,setTitle]= useState('');
     const [body, setBody] =useState('');
-    const [author,setAuthor]=useState('Mario');
+    const [author,setAuthor]=useState('');
     const [isPending,setIsPending]=useState(false);
     const history = useHistory();
 
@@ -41,13 +41,19 @@ const Create = () => {
                     onChange= {(e)=>setBody(e.target.value)}
                  ></textarea>
                  <label>Blog Author: </label>
-                <select
+                {/* <select
                     value={author}
                     onChange= {(e)=>setAuthor(e.target.value)}
                 >
                     <option value="Mario">Mario</option>
                     <option value="Kshipra">Kshipra</option>
-                </select>
+                </select> */}
+                <input 
+                    type="text" 
+                    required
+                    value={author}
+                    onChange= {(e)=>setAuthor(e.target.value)}
+                    />
                 {!isPending && <button>Add Blog</button>}
                 {isPending && <button disabled>Adding blog...</button>}
                     
